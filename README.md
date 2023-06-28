b89dc59334f4e1af13122c9c0ee9126d88390815
Jenish Kothari <kotharijenish2001@gmail.com>
2023-06-28 08:43:55 +0530

    Episode-2 & Episode-3

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 Namaste React Series - Episode 1
+
+# Parcel 
+1. Creating Dev Build 
+2. Local Server
+3. Hot module Replacement  ( HTML )
+4. File Watching Algorithm - written in C++ it keeps look on all of the files
+5. Parcel also does caching and hence for each builds it may reduce build time
+6. Bundling
+7. Compressing
+8. Consistent hashing
+9. Code Splitting
+10. Differential Bundling
+11. Diagnostics and Error Handling
+12. Parcel Gives you also HTTPS 
+13. Tree Shaking - Remove unused Code
--- app.js
+++ app.js
@@ -14,17 +14,39 @@
 *
 *
 */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent =
-    React.createElement('div', { id: "parent" },[ React.createElement('div', { id: "child" }, [React.createElement('h1', { id: "heading" }, "Hello World from React"),
-    React.createElement('h2', { id: "heading" }, "Hello World from React")]),
-    React.createElement('div', { id: "child2" }, [React.createElement('h1', { id: "heading" }, "Hello World from React"),
-    React.createElement('h2', { id: "heading" }, "Hello World from React")])
+// const parent =
+//     React.createElement('div', { id: "parent" },[ React.createElement('div', { id: "child" }, [React.createElement('h1', { id: "heading" }, "Hello World from React"),
+//     React.createElement('h2', { id: "heading" }, "Hello World from React")]),
+//     React.createElement('div', { id: "child2" }, [React.createElement('h1', { id: "heading" }, "Hello World from React"),
+//     React.createElement('h2', { id: "heading" }, "Hello World from React")])
 
-])
+// ])
 
 // To much Tidy Code => There comes JSX
 // JSX => JavaScript XML
 
-const root1 = ReactDOM.createRoot(document.getElementById('root'))
-root1.render(parent)
+// const root1 = ReactDOM.createRoot(document.getElementById('root'))
+// root1.render(parent)
+
+// const heading = React.createElement('h1',{id:"heading"},"Hello World from React")
+
+// root.render(heading)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const elem = (<span>Span React Element</span>);
+
+const Title = () => <h1 className="heading">Namaste React using JSX</h1>;
+
+const Heading = () => (
+  <div id="container">
+    <Title />
+    {elem}
+    <h1 className="heading">Heading Functional Component</h1>
+  </div>
+);
+
+root.render(<Heading />);
