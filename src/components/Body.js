5b79b24828aac417aaf37bfd5886869667a489a9
Jenish Kothari <kotharijenish2001@gmail.com>
2023-07-17 10:24:25 +0530

    Namaste React - Hooks

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,17 +1,29 @@
-import RestaurantCard from "./RestaurantCard";
-import resObj  from '../../utils/mockData'
-const Body = () => (
-    <div className="body">
-      <div className="search">Search</div>
-      <div className="res-container">
-        {
-          resObj.map(restaurant=>(
-            <RestaurantCard key= {restaurant.data.id} resData = {restaurant}/>
-          ))
-        }
-        
-      </div>
-    </div>
-  );
 
-  export default Body;
+import RestaurantCard from "./RestaurantCard";
+import resObj from "../../utils/mockData";
+import {useState} from "react";
+
+
+const Body = () => {
+  const [ restaurants, setRestaurants ] = useState(resObj);
+  return (
+  <div className="body">
+    <div className="filter">
+      <button className="filter-btn" onClick = {()=>{
+        const filteredRestaurants = restaurants.filter((res)=>res.data.avgRating>4)
+        setRestaurants(filteredRestaurants)
+      }}>
+
+        Top Rated Restaurants
+      </button>
+    </div>
+    <div className="res-container">
+      {restaurants.map((restaurant) => (
+        <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+      ))}
+    </div>
+  </div>
+  )
+};
+
+export default Body;
